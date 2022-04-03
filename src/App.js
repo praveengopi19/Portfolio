30bb56c0f69858cdc4c72175c3e26e34c5f53b40
praveengopi19 <praveengopi16@gmail.com>
2022-04-03 10:55:24 +0530

    enhancement: react-router upgrade

--- src/App.js
+++ src/App.js
@@ -1,51 +1,53 @@
 import { lazy, Suspense } from 'react';
-import { Switch, Route, Link } from 'react-router-dom';
+import { Routes as Switch, Route } from 'react-router-dom';
 
-import { ReactComponent as Error404 } from './assets/icons/error.svg'
+import { ReactComponent as Error404 } from './assets/icons/error.svg';
 import ErrorBoundries from './components/ErrorBoundries';
 
-//import DesktopMain from './components/Desktop/DesktopMain';
-//import MobileMain from './components/Mobile/MobileMain';
+const DesktopMain = lazy(() => import('./components/Desktop/DesktopMain'));
+const MobileMain = lazy(() => import('./components/Mobile/MobileMain'));
 
-const DesktopMain = lazy(() => import("./components/Desktop/DesktopMain"));
-const MobileMain = lazy(() => import("./components/Mobile/MobileMain"));
-
-
-const Error = () => (<section className="sectionClass"><div className="error404"><Error404 /></div>
-  <h5>Oh no! The requested URL was not found</h5>
-  <a href="/"><button className="errorbuttom">Return Home</button></a></section>);
+function Error() {
+  return (
+    <section className="sectionClass">
+      <div className="error404"><Error404 /></div>
+      <h5>Oh no! The requested URL was not found</h5>
+      <a href="/"><button type="button" className="errorbuttom">Return Home</button></a>
+    </section>
+  );
+}
 
 function App() {
-  var ua = navigator.userAgent;
-  var mobile = /IEMobile|Windows Phone|Lumia/i.test(ua) ? 'w' : /iPhone|iP[oa]d/.test(ua) ? 'i' : /Android/.test(ua) ? 'a' : /BlackBerry|PlayBook|BB10/.test(ua) ? 'b' : /Mobile Safari/.test(ua) ? 's' : /webOS|Mobile|Tablet|Opera Mini|\bCrMo\/|Opera Mobi/i.test(ua) ? 1 : 0;
+  const ua = navigator.userAgent;
+  const mobile = /IEMobile|Windows Phone|Lumia/i.test(ua) ? 'w' : /iPhone|iP[oa]d/.test(ua) ? 'i' : /Android/.test(ua) ? 'a' : /BlackBerry|PlayBook|BB10/.test(ua) ? 'b' : /Mobile Safari/.test(ua) ? 's' : /webOS|Mobile|Tablet|Opera Mini|\bCrMo\/|Opera Mobi/i.test(ua) ? 1 : 0;
   return (
-    <>
-      <Suspense fallback={<section className="sectionClass">
-        <svg className="spinner" width="174px" height="174px" viewBox="0 0 66 66" >
+    <Suspense fallback={(
+      <section className="sectionClass">
+        <svg className="spinner" width="174px" height="174px" viewBox="0 0 66 66">
           <circle className="path" fill="transparent" strokeWidth="2" cx="33" cy="33" r="11" stroke="rgb(61, 157, 209)" />
         </svg>
         <div className="loader">Preparing Portfolio</div>
-      </section>}>
-        <ErrorBoundries>
-          {mobile ?
+      </section>
+)}
+    >
+      <ErrorBoundries>
+        {mobile
+          ? (
             <Switch>
-              <Route exact path="/" render={(props) => (<MobileMain mobile={mobile} />)} />
-              <Route component={() => (<Error />)} />
+              <Route exact path="/" element={<MobileMain mobile={mobile} />} />
+              <Route component={Error} />
             </Switch>
-            :
+          )
+          : (
             <Switch>
-              <Route exact path="/" render={props => (<MobileMain mobile={mobile} />)} />
-              <Route exact path="/cmd" render={props => (<DesktopMain mobile={mobile} />)} />
-              <Route component={() => (<Error />)} />
+              <Route exact path="/" element={<MobileMain mobile={mobile} />} />
+              <Route exact path="/cmd" component={DesktopMain} />
+              <Route component={Error} />
             </Switch>
-          }
-        </ErrorBoundries>
-      </Suspense>
-    </ >
+          )}
+      </ErrorBoundries>
+    </Suspense>
   );
 }
 
 export default App;
-//header flex inital
-//content flex 1 1 basis
-//footer flex end
--- src/index.js
+++ src/index.js
@@ -5,23 +5,6 @@ import './App.css';
 import { HashRouter as Router } from 'react-router-dom';
 import App from './App';
 
-// Router
-
-// {import '!!file-loader?name=./manifest.json!../public/manifest.json'
-// import '!!file-loader?name=./icon-192x192.png!../public/icon-192x192.png'
-// import '!!file-loader?name=./icon-256x256.png!../public/icon-256x256.png'
-// import '!!file-loader?name=./icon-384x384.png!../public/icon-384x384.png'
-// import '!!file-loader?name=./icon-512x512.png!../public/icon-512x512.png'
-
-// import '!!file-loader?name=./offline.html!../public/offline.html'
-
-// import 'file-loader?name=./sw.js!./sw.js'
-
-// import '!!file-loader?name=./pk.svg!../public/pk.svg'
-// import '!!file-loader?name=./ogcmd.png!../public/ogcmd.png'
-
-// import worker from "worker-loader!./sw.js"}
-
 import worker from './sw.js';
 
 // create centralised store
@@ -38,8 +21,49 @@ ReactDOM.render(
 let isOnline = ('onLine' in navigator) && navigator.onLine;
 
 if ('serviceWorker' in navigator) {
-  var svcWorker;
-  var svcResgisteration;
+  let svcWorker;
+  let svcResgisteration;
+
+  const sendMsgtoSW = async (msg, target) => {
+    if (target) {
+      target.postMessage(msg);
+    } else if (svcWorker) {
+      svcWorker.postMessage(msg);
+    } else {
+      navigator.serviceWorker.controller.postMessage(msg);
+    }
+  };
+
+  const sendStatusUpdate = async (target) => {
+    sendMsgtoSW({ statusUpdate: { isOnline } }, target);
+  };
+
+  const onMessage = async (event) => {
+    const { data } = event;
+
+    if (data.requestStatusUpdate) {
+      sendStatusUpdate(event.ports && event.ports[0]);
+    }
+  };
+
+  const swInit = async () => {
+    svcResgisteration = await navigator.serviceWorker.register('./sw.js', {
+      updateViaCache: 'none',
+    });
+
+    svcWorker = svcResgisteration.installing || svcResgisteration.waiting
+     || svcResgisteration.active;
+
+    sendStatusUpdate(svcWorker);
+    console.log("Hi from praveengopi19.github.io's service worker");
+
+    navigator.serviceWorker.addEventListener('controllerchange', () => {
+      svcWorker = navigator.serviceWorker.controller;
+      sendStatusUpdate(svcWorker);
+    });
+
+    navigator.serviceWorker.addEventListener('message', onMessage);
+  };
 
   window.addEventListener('online', () => {
     isOnline = true;
@@ -52,43 +76,4 @@ if ('serviceWorker' in navigator) {
   });
 
   swInit().catch(console.error());
-}
-
-async function swInit() {
-  svcResgisteration = await navigator.serviceWorker.register('./sw.js', {
-    updateViaCache: 'none',
-  });
-
-  svcWorker = svcResgisteration.installing || svcResgisteration.waiting || svcResgisteration.active;
-  sendStatusUpdate(svcWorker);
-  console.log("Hi from praveengopi19.github.io's service worker");
-
-  navigator.serviceWorker.addEventListener('controllerchange', (event) => {
-    svcWorker = navigator.serviceWorker.controller;
-    sendStatusUpdate(svcWorker);
-  });
-
-  navigator.serviceWorker.addEventListener('message', onMessage);
-}
-
-async function onMessage(event) {
-  const { data } = event;
-
-  if (data.requestStatusUpdate) {
-    sendStatusUpdate(event.ports && event.ports[0]);
-  }
-}
-
-async function sendStatusUpdate(target) {
-  sendMsgtoSW({ statusUpdate: { isOnline } }, target);
-}
-
-async function sendMsgtoSW(msg, target) {
-  if (target) {
-    target.postMessage(msg);
-  } else if (svcWorker) {
-    svcWorker.postMessage(msg);
-  } else {
-    navigator.serviceWorker.controller.postMessage(msg);
-  }
 }
